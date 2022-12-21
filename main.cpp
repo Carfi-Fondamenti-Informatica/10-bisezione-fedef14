417e21ec72931215f6e55d7586a8735e168be527
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-12-21 12:59:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include "cmath"
 using namespace std;
 
-int main() {
+float f(float y){
+    return (pow(y,2)*cos(y))+1;
+}
+
+int main(){
+   float a = 0, b = 0, x = 0, err = 0;
+    do{
+        cout << "inserire estremi" << endl;
+        cin >> a;
+        cin >> b;
+    }while(f(a)*f(b) >= 0);
+
+    if(f(a)<f(b)){ 
+        b = a+b;
+        a = b-a;
+        b = b-a;
+    }
+do {
+    x = (a + b) / 2;
+
+    if (f(x) == 0) {
+        break;
+    }
+    if (f(x) > 0) {
+        a = x;
+    } else {
+        b = x;
+    }
+    err = abs((b-a)/2);
+} while(err >= pow(M_E,-6));
+
+
+    int i = x * 1000;
+    double l = (float) i/1000;
+    cout << l << endl;
+   
+   
+   
    return 0;
 }
